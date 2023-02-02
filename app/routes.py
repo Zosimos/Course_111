73776e745f672a0aab174279483e89ed0c0eb7bb
Jim Barnett <hlbradow@yahoo.com>
2023-02-01 19:31:52 -0500

    111 first commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask         #from the flask module import the flask class
+app = Flask(__name__)           #create an instance of the flask class into the app variable (now an object) - "class is to object, like blueprint is to house"
+
+
+@app.get("/")   #Flask decorator that creates routes. decorator is a function that wraps another function.
+def index():    #flask calls these "view functions". A wrapped function is referred to as view functions.
+    me = {      #python dictionary with key/value pairs. Before colon is key, after colon is the value.
+        "first_name": "Jim",
+        "last_name": "Barnett",
+        "hobbies": "Weightlifting",
+        "is_active": True
+    }
+
+    return me   #when a view function returns a dict. Flask automatically converts it to JSON.
